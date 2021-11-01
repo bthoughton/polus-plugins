6632bea97d71df6cfa82f607c3cfd1b14903b4c3
Benjamin Houghton <bth51@georgetown.edu>
2021-11-01 16:37:52 +0000

    Commit pending PR updates

--- utils/polus-imagej-util/{{cookiecutter.project_slug}}/src/ij_converter.py
+++ utils/polus-imagej-util/{{cookiecutter.project_slug}}/src/ij_converter.py
@@ -63,16 +63,16 @@ def _java_setup():
         'uint32'     : imglyb.types.UnsignedIntType,
         'uint64'     : imglyb.types.UnsignedLongType
     }
-    # PRIMITIVES = {
-    #     'double'     : jpype.JDouble,
-    #     'float'      : jpype.JFloat,
-    #     'long'       : jpype.JLong,
-    #     'int'        : jpype.JInt,
-    #     'short'      : jpype.JShort,
-    #     'char'       : jpype.JChar,
-    #     'byte'       : jpype.JByte,
-    #     'boolean'    : jpype.JBoolean
-    # }
+    PRIMITIVES = {
+        'double'     : jpype.JDouble,
+        'float'      : jpype.JFloat,
+        'long'       : jpype.JLong,
+        'int'        : jpype.JInt,
+        'short'      : jpype.JShort,
+        'char'       : jpype.JChar,
+        'byte'       : jpype.JByte,
+        'boolean'    : jpype.JBoolean
+    }
     # PRIMITIVES = {
     #     'double'     : jpype.JDouble,
     #     'float'      : jpype.JFloat,
@@ -97,24 +97,24 @@ def _java_setup():
 scyjava.when_jvm_starts(_java_setup)
 
 JAVA_CONVERT = {}
-# JAVA_CONVERT.update({
-#     t:lambda s,t,st: IMGLYB_PRIMITIVES[str(st)](st.type(s)) for t in ABSTRACT_SCALARS
-# })
-# JAVA_CONVERT.update({
-#     t:lambda s,t,st: PRIMITIVES[t](float(s)) for t in SCALARS
-# })
+JAVA_CONVERT.update({
+    t:lambda s,t,st: IMGLYB_PRIMITIVES[str(st)](st.type(s)) for t in ABSTRACT_SCALARS
+})
+JAVA_CONVERT.update({
+    t:lambda s,t,st: PRIMITIVES[t](float(s)) for t in SCALARS
+})
 JAVA_CONVERT.update({
     t:lambda s,t,st: PRIMITIVE_ARRAYS[t]([float(si) for si in s.split(',')]) for t in ARRAYS
 })
-JAVA_CONVERT.update({
-    t: lambda s,t,st: IMGLYB_PRIMITIVES[str(st)](s) for t in SCALARS
-})
+# JAVA_CONVERT.update({
+#     t: lambda s,t,st: IMGLYB_PRIMITIVES[str(st)](s) for t in SCALARS
+# })
 
 JAVA_CONVERT.update({
-    t:lambda s: imglyb.util.Views.iterable(ij.py.to_java(s)) for t in ABSTRACT_ITERABLES
+    t:lambda s,ij: imglyb.util.Views.iterable(ij.py.to_java(s)) for t in ABSTRACT_ITERABLES
 })
 JAVA_CONVERT.update({
-    t:lambda s: imglyb.util._to_imglib(s) for t in IMG_ARRAYS
+    t:lambda s,ij: imglyb.util._to_imglib(s) for t in IMG_ARRAYS
 })
 
 def to_java(ij, np_array,java_type,java_dtype=None):
@@ -129,7 +129,7 @@ def to_java(ij, np_array,java_type,java_dtype=None):
         if str(java_dtype) != 'None':
             out_array = JAVA_CONVERT[java_type](np_array,java_type,java_dtype)
         else:
-            out_array = JAVA_CONVERT[java_type](np_array)
+            out_array = JAVA_CONVERT[java_type](np_array, ij)
     else:
         logger.warning('Did not recognize type, {}, will pass default.'.format(java_type))
         out_array = ij.py.to_java(np_array)
